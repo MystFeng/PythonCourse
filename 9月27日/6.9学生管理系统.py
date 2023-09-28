a97fd37fdc93be21934f3b7f708d6b8dec32c70c
Oliver <86922523+MystFeng@users.noreply.github.com>
2023-09-28 09:43:57 +0000

    加了注释

--- 9月27日/6.9学生管理系统.py
+++ 9月27日/6.9学生管理系统.py
@@ -1,16 +1,5 @@
-"""
-使用自定义的函数，完成对程序的模块化
-学生信息包含：姓名、性别、手机号，参考格式如下：
-[{"name":"张三","sex":"男","phone":123456},{"name":"李四","sex":"男","phone":654321}]
-该系统具有的功能：添加、删除、修改、显示、退出系统
-设计思路：
-提示用户选择功操作
-获取用户选择的功能
-根据用户的选择，分别调用不同的函数
-"""
-
-# 存储学生信息列表
 students = [
+    # 学生信息模板
     {'name': '张三', 'phone': '12345', 'sex': '女'},
     {'name': '李四', 'phone': '67890', 'sex': '男'},
     {'name': '蔡徐坤', 'phone': '110', 'sex': '鸡你太美'},
@@ -19,10 +8,12 @@ students = [
 
 
 def print_stu_info(student):
+    """规格化打印学生信息，接收1个学生单位"""
     print(f"姓名：{student['name']}，电话：{student['phone']}，性别：{student['sex']}")
 
 
 def print_menu():
+    """输出菜单"""
     print(
         '————————————————\n'
         '学生管理系统 V10.0\n'
@@ -35,6 +26,7 @@ def print_menu():
 
 
 def add_stu_info():
+    """添加学生信息"""
     new_student = {
         'name': input('请输入学生姓名: '),
         'phone': input('请输入学生ID: '),
@@ -45,6 +37,7 @@ def add_stu_info():
 
 
 def del_stu_info():
+    """删除学生信息"""
     del_student_name = input('要删除哪位学生（姓名）？')
     for student in list(students):
         if student['name'] == del_student_name:
@@ -56,6 +49,7 @@ def del_stu_info():
 
 
 def mod_stu_info():
+    """修改学生信息"""
     mod_student_name = input('要修改哪位学生（姓名）？')
     for student in students:
         if student['name'] == mod_student_name:
@@ -71,7 +65,8 @@ def mod_stu_info():
         print('找不到这名学生。')
 
 
-def show_stu_info():
+def show_all_stu_info():
+    """输出所有学生信息"""
     if not students:
         print('学生列表为空。')
     else:
@@ -81,6 +76,7 @@ def show_stu_info():
 
 
 def main():
+    """主要函数 """
     while True:
         print_menu()
         key = input("请输入功能对应的数字：")
@@ -95,7 +91,7 @@ def main():
             mod_stu_info()
         # 查询所有学生信息
         elif key == '4':
-            show_stu_info()
+            show_all_stu_info()
         # 退出系统
         elif key == '0':
             quit_confirm = input('确实要退出吗(Y/N)？').lower()
--- 9月27日/实验4.3.py
+++ 9月27日/实验4.3.py
@@ -1,3 +1,6 @@
+from 9月27日.6.9学生管理系统 import add_stu_info, del_stu_info, mod_stu_info, print_menu, show_stu_info
+
+
 str = 'ppssAQWssd'
 
 
@@ -15,3 +18,31 @@ def count(str):
 if __name__ == "__main__":
     result = count(str)
     print(result)
+
+
+def main():
+    while True:
+        print_menu()
+        key = input("请输入功能对应的数字：")
+        # 添加学生信息
+        if key == '1':
+            add_stu_info()
+        # 删除学生信息
+        elif key == '2':
+            del_stu_info()
+        # 修改学生信息
+        elif key == '3':
+            mod_stu_info()
+        # 查询所有学生信息
+        elif key == '4':
+            show_stu_info()
+        # 退出系统
+        elif key == '0':
+            quit_confirm = input('确实要退出吗(Y/N)？').lower()
+            if quit_confirm == 'y':
+                print("再见!")
+                break
+            elif quit_confirm == 'n':
+                continue
+            else:
+                print('输入错误。')
