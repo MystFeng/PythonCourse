15ff3eb707030b848dad65ee480a43dbb044312d
MystFeng <86922523+MystFeng@users.noreply.github.com>
2023-09-28 14:07:31 +0800

    搬走所有文件

--- 实验1.2.py
+++ 实验1.2.py
@@ -5,7 +5,7 @@ if '#2#' in b:
     start_index = b.index('#2#') + len('#2#')
     end_index = b.index('end')
     middle_string = b[start_index:end_index]
-    result = a+' '+middle_string
+    result = a + ' ' + middle_string
 else:
     result = 'nullnullnull'
 print(result)
