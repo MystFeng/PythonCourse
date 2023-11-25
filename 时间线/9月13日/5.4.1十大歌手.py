30efb9fc22796f87b0b2d910161a59f301a69279
Oliver <delicacyfeng2580@outlook.com>
2023-11-25 23:16:34 +0800

    commit

--- 时间线/9月13日/5.4.1十大歌手.py
+++ 时间线/9月13日/5.4.1十大歌手.py
@@ -1,11 +1,11 @@
-def calculate_score(scores):
-    sorted_scores = sorted(scores)
-    trimmed_scores = sorted_scores[1:-1]
+def calculate_average_score(scores):
+    trimmed_scores = sorted(scores)[1:-1]
+    print(trimmed_scores)
     average_score = sum(trimmed_scores) / len(trimmed_scores)
     return average_score
 
 
-def get_judge_scores():
+def get_scores():
     scores = []
     for i in range(10):
         scores.append(float(input(f"请输入第{i + 1}位评委的打分：")))
@@ -13,7 +13,7 @@ def get_judge_scores():
 
 
 def main():
-    average_score = calculate_score(get_judge_scores())
+    average_score = calculate_average_score(get_scores())
     print(f"该选手的最终得分是：{average_score}")
 
 
---
+++ 期末/13替换python为Python.py
@@ -0,0 +1,4 @@
+strings = "Life is short. I use python."
+if "python" in strings:
+    strings = strings.replace("python", "Python")
+print(f"替换：{strings}")
