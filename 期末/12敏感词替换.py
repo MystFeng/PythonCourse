284017ccb8de3d24e9294d079b7a0f090cfae168
Oliver <delicacyfeng2580@outlook.com>
2023-11-25 23:02:47 +0800

    commit

--- 期末/12敏感词替换.py
+++ 期末/12敏感词替换.py
@@ -1,5 +1,12 @@
-bad_words = ["傻逼", "智障", "奶子", "做爱", "蠢", "滚", "去死", "有病", "操", "你妈"]
-user_input = input("骂人试试：")
+bad_words = ["傻逼", "智障", "蠢", "滚", "死", "操", "你妈", "狗东西"]
+original_input = input("骂人试试：")
+clean_input = original_input
+
 for word in bad_words:
-    user_input = user_input.replace(word, "*" * len(word)) if word in user_input else None
-print(f"脏话已替换为{user_input}")
+    if word in clean_input:
+        clean_input = clean_input.replace(word, "*" * len(word))
+
+if clean_input == original_input:
+    print("未检测到敏感词。")
+else:
+    print(f"脏话已被替换为：{clean_input}")
