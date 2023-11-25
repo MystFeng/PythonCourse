efce7ae551112498506c8592a0e8e636b7020538
Oliver <delicacyfeng2580@outlook.com>
2023-11-25 15:01:43 +0800

    commit

---
+++ 期末/7运煤.py
@@ -0,0 +1,4 @@
+total = 29.5
+a = 4 * 3
+times = (total - a) / 2.5
+print(times)
--- 期末/8计算器.py
+++ 期末/8计算器.py
@@ -8,8 +8,6 @@ def calculate(x, y, operator):
     return operations[operator] if operator in operations else print("运算符错误")
 
 
-
-
 while True:
     n1 = float(input("请输入第一个数字："))
     n2 = float(input("请输入第二个数字："))
