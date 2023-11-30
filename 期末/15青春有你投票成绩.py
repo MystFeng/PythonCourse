94ddd041ba1b27bb97830ef723776fc89395028c
Oliver <delicacyfeng2580@outlook.com>
2023-11-30 20:27:56 +0800

    commit

--- 期末/15青春有你投票成绩.py
+++ 期末/15青春有你投票成绩.py
@@ -1,12 +1,13 @@
-players = {}
-list = []
+scores = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10}
 while True:
     name = input("请输入选手名字(按Q退出)：")
-    score = float(input("请输入选手成绩(按Q退出)："))
-    if name.lower() == 'q' or score.lower() == 'q':
+    if name.lower() == 'q':
         break
-    players[name] = score
+    score = float(input("请输入选手成绩："))
+    scores[name] = score
 
-sorted_players = sorted(players.items(), key=lambda x: x[1], reverse=True)
-for i, (name, score) in enumerate(sorted_players, 1):
-    print(f"第{i}名：{name}，成绩为{score}分")
+sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+print(sorted_scores)
+
+for i in range(len(sorted_scores)):
+    print(f'第{i + 1}名：{sorted_scores[i][0]}，成绩为{sorted_scores[i][1]}分')
