57e6e39cd0e202017bbdf16d9ddf72ed6bff5800
Oliver <delicacyfeng2580@outlook.com>
2023-12-01 22:30:37 +0800

    commit

--- 期末/19(P71,T4)删除列表重复数据.py
+++ 期末/19(P71,T4)删除列表重复数据.py
@@ -1,1 +1,14 @@
+# 常规for方法
+# li_one = [1, 2, 1, 2, 3, 5, 4, 3, 5, 7, 4, 7, 8]
+# li_two=[]
+# for i in li_one:
+#     if i in li_two:
+#         pass
+#     else:
+#         li_two.append(i)
+# print(li_two)
+
+# 利用集合的不重复性（更简洁）
 li_one = [1, 2, 1, 2, 3, 5, 4, 3, 5, 7, 4, 7, 8]
+li_two = list(set(li_one))
+print(li_two)
