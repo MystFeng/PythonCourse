62528feca8191f9b430289e4f9ecf25e85867095
Oliver <delicacyfeng2580@outlook.com>
2023-12-03 00:09:55 +0800

    commit

--- 期末/实验题/实验1,T2.py
+++ 期末/实验题/实验1,T2.py
@@ -1,11 +1,12 @@
 a = 'Hello'
 b = '#2#Lisaend'
-result = ''
+
 if '#2#' in b:
     start = b.index('#2#') + len('#2#')
     end = b.index('end')
     middle = b[start:end]
     result = a + ' ' + middle
+    print(result)
 else:
     result = 'null'
-print(result)
+# index方法是用来检测元素在变量中首次出现时的位置。
