cc0737cf4e9d0070df3c81a57396c799e0591853
Oliver <delicacyfeng2580@outlook.com>
2023-12-03 10:55:19 +0800

    commit

--- 期末/实验题/实验2,T1.py
+++ 期末/实验题/实验2,T1.py
@@ -1,8 +1,12 @@
 user_input = input("请输入一个字符串: ")
 # 提取下标为偶数的字符
-list_A = [user_input[i] for i in range(0, len(user_input), 2)]
+list1 = []
+for i in range(0, len(user_input), 2):
+    list1.append(user_input[i])
 # 提取下标为奇数的字符
-list_B = [user_input[i] for i in range(1, len(user_input), 2)]
+list2 = []
+for i in range(1, len(user_input), 2):
+    list2.append(user_input[i])
 # 将A和B连接起来
-combined_list = list_A + list_B
-print("合并后的列表:", combined_list)
+combined_list = list1 + list2
+print(f"合并后的列表：{combined_list}")
--- 期末/实验题/实验3,T1.py
+++ 期末/实验题/实验3,T1.py
@@ -1,11 +1,29 @@
 # 用户输入月份，判断这个月是哪个季节。
-# 3，4，5月----春季  6，7，8----夏季   9，10，11---秋季  12，1，2----冬季
+# 3、4、5月----春季，6、7、8----夏季，9、10、11---秋季，12、1、2----冬季
 
-season = {'春季': {3, 4, 5}, '夏季': {6, 7, 8}, '秋季': {9, 10, 11}, '冬季': {12, 1, 2}}
+# 用字典的方式
+seasons = {'春季': [3, 4, 5],
+           '夏季': [6, 7, 8],
+           '秋季': [9, 10, 11],
+           '冬季': [12, 1, 2]}
+
 input_month = int(input('输入要判断的月份：'))
-for key, value in season.items():
-    if input_month in value:
-        print(f'这个月份在{key}')
+for season, months in seasons.items():
+    if input_month in months:
+        print(f'这个月份是{season}')
         break
 else:
     print('输入错误！')
+
+# 用if-elif-else语句
+# input_month = int(input('输入要判断的月份：'))
+# if input_month in {3, 4, 5}:
+#     print('这个月份是春季')
+# elif input_month in {6, 7, 8}:
+#     print('这个月份是夏季')
+# elif input_month in {9, 10, 11}:
+#     print('这个月份是秋季')
+# elif input_month in {12, 1, 2}:
+#     print('这个月份是冬季')
+# else:
+#     print('输入错误！')
