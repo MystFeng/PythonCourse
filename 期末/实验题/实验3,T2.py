ca75766c3265d007268d732628e51b06d2a8c3ee
Oliver <delicacyfeng2580@outlook.com>
2023-12-06 20:38:33 +0800

    commit

--- 期末/实验题/实验3,T2.py
+++ 期末/实验题/实验3,T2.py
@@ -3,3 +3,9 @@ new_info = {}
 for name, gender in sinfo.items():
     new_info[name] = gender if gender == 0 else None
 print(f"修改后的的员工信息为：{new_info}")
+
+# 其他方法：
+# sinfo = {"xiaohong": 0, "xiaolan": 0, "xiaoming": 1, "xiaobai": 1}
+# filtered_sinfo = {name: gender for name, gender in sinfo.items() if gender == 0}
+# print(f"修改前的信息:{sinfo}")
+# print(f"修改后的信息:{filtered_sinfo}")
--- 期末/实验题/实验4,T2.py
+++ 期末/实验题/实验4,T2.py
@@ -1,11 +1,11 @@
-def update_info(sinfo):
-    for name, gender in list(sinfo.items()):
-        sinfo.pop(name) if gender == 1 else None
-    return sinfo
+sinfo = {"xiaohong": 0, "xiaolan": 0, "xiaoming": 1, "xiaobai": 1}
+new_info = {}
+for name, gender in sinfo.items():
+    new_info[name] = gender if gender == 0 else None
+print(f"修改后的的员工信息为：{new_info}")
 
-
-if __name__ == "__main__":
-    sinfo = {"xiaohong": 0, "xiaolan": 0, "xiaoming": 1, "xiaobai": 1}
-    print(f"修改前的信息:{sinfo}")
-    sinfo = update_info(sinfo)
-    print(f"修改后的信息:{sinfo}")
+# 其他方法：
+# sinfo = {"xiaohong": 0, "xiaolan": 0, "xiaoming": 1, "xiaobai": 1}
+# filtered_sinfo = {name: gender for name, gender in sinfo.items() if gender == 0}
+# print(f"修改前的信息:{sinfo}")
+# print(f"修改后的信息:{filtered_sinfo}")
